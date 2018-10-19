ae7a9496266dc458ffd40141a8619f320c7c92b2
Yiying Wang <madlogos@users.noreply.github.com>
2018-10-19 16:04:54 +0800

    C5 wk2

--- 5_Sequence Models/emo_utils.py
+++ 5_Sequence Models/emo_utils.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import csv
+import codecs
 import numpy as np
 import emoji
 import pandas as pd
@@ -6,7 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 def read_glove_vecs(glove_file):
-    with open(glove_file, 'r') as f:
+    with open(glove_file, 'r', encoding='utf-8') as f:
         words = set()
         word_to_vec_map = {}
         for line in f:
--- 5_Sequence Models/w2v_utils.py
+++ 5_Sequence Models/w2v_utils.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import codecs
 from keras.models import Model
 from keras.layers import Input, Dense, Reshape, merge
 from keras.layers.embeddings import Embedding
@@ -99,7 +102,7 @@ class SimilarityCallback:
     
 
 def read_glove_vecs(glove_file):
-    with open(glove_file, 'r') as f:
+    with open(glove_file, 'r', encoding='utf-8') as f:
         words = set()
         word_to_vec_map = {}
         
